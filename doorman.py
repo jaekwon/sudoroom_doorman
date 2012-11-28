f855cca77ef2dff2cda7c26ebed84a0c651d0fa9
Jae Kwon <jkwon.work@gmail.com>
2012-11-27 23:45:21 -0800

    parametrized thresholds.

--- doorman.py
+++ doorman.py
@@ -5,10 +5,14 @@ import wave
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-MEASURE_SECONDS = 5
+CHANNELS = 1          # Make sure the audio input goes into the right channel (left/right)
+RATE = 44100          # This is some factor more than our theoretical baudrate. We should also look into telephony software to accept dial tones for easy digial binary transfer.
+MEASURE_SECONDS = 5   # Change this to make the script more or less sensitive to temporary pulses
 MEASURE_SAMPLES = MEASURE_SECONDS * RATE
+
+# Note: We just get the abs(sample) value, which is fine for square waves, but may not be for sinusoidal waves.
+ON_THRESHOLD = 20000  # Note that the sample format is a signed 16bit int. If the sample amplitudes are higher than this, switch to 'on' state.
+OFF_THRESHOLD = 10000 # If the sample amplitudes are lower than this, switch to 'off' state.
 
 p = pyaudio.PyAudio()
 
@@ -35,10 +39,10 @@ while True:
     sampleAvgish = sampleAbsSum / float(sampleCount)
     sampleCount = 0
     sampleAbsSum = 0.0
-    if sampleAvgish > 20000 and state is "OFF":
+    if sampleAvgish > ON_THRESHOLD and state is "OFF":
       print "ON"
       state = "ON"
-    elif sampleAvgish < 10000 and state is "ON":
+    elif sampleAvgish < OFF_THRESHOLD and state is "ON":
       print "OFF"
       state = "OFF"
 
