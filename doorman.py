c98e6c6887e5d4cee22fad0b8d2019ab69574205
notconfusing <isalix@gmail.com>
2012-11-29 21:20:01 -0800

    claptastic

--- doorman.py
+++ doorman.py
@@ -7,12 +7,12 @@ CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 1          # Make sure the audio input goes into the right channel (left/right)
 RATE = 44100          # This is some factor more than our theoretical baudrate. We should also look into telephony software to accept dial tones for easy digial binary transfer.
-MEASURE_SECONDS = 5   # Change this to make the script more or less sensitive to temporary pulses
+MEASURE_SECONDS = .05   # Change this to make the script more or less sensitive to temporary pulses
 MEASURE_SAMPLES = MEASURE_SECONDS * RATE
 
 # Note: We just get the abs(sample) value, which is fine for square waves, but may not be for sinusoidal waves.
-ON_THRESHOLD = 20000  # Note that the sample format is a signed 16bit int. If the sample amplitudes are higher than this, switch to 'on' state.
-OFF_THRESHOLD = 10000 # If the sample amplitudes are lower than this, switch to 'off' state.
+ON_THRESHOLD =  1000.0  # Note that the sample format is a signed 16bit int. If the sample amplitudes are higher than this, switch to 'on' state.
+OFF_THRESHOLD = 800.0 # If the sample amplitudes are lower than this, switch to 'off' state.
 
 p = pyaudio.PyAudio()
 
@@ -28,23 +28,37 @@ state = "OFF"
 sampleCount = 0
 sampleAbsSum = 0.0
 
+def distanceBetweenClaps(clapOne, clapTwo)
+  """Reports the positive definite amount of time between clapOne and ClapTwo)"""
+  distance = 0
+  return distance
+
+def recognizePattern(clapList)
+  #TODO
+  return False
+
+clapList = []
+
 while True:
-  data = stream.read(CHUNK)
-  samples = wave.struct.unpack("%dh"%(len(data)/2), data)
+  data = stream.read(CHUNK) #get a CHUNK's worth of data, which is CHUNK number of samples
+  samples = wave.struct.unpack("%dh"%(len(data)/2), data) #destructure a binary sting into a list of sample integers
   for sample in samples:
-    sampleCount += 1
-    sampleAbsSum += abs(sample)
+    sampleCount += 1 
+    sampleAbsSum += abs(sample) #remove negativity
+  #enter this next condition once MEASURE_SAMPLEs has elapsed
   if sampleCount >= MEASURE_SAMPLES:
-    print sampleCount, sampleAbsSum
-    sampleAvgish = sampleAbsSum / float(sampleCount)
-    sampleCount = 0
-    sampleAbsSum = 0.0
+    sampleAvgish = sampleAbsSum / float(sampleCount) #calculate the average "amplitude" (postive definite metric)
+    #print sampleCount, sampleAvgish, (sampleAvgish > ON_THRESHOLD),  (sampleAvgish < OFF_THRESHOLD)
     if sampleAvgish > ON_THRESHOLD and state is "OFF":
-      print "ON"
-      state = "ON"
-    elif sampleAvgish < OFF_THRESHOLD and state is "ON":
-      print "OFF"
-      state = "OFF"
+      print "Clapped"
+      clapList.append(datetime.datetime.now()) #add a clap event date to our list
+      if len(clapList) > 100: #see if it's full
+        clapList.pop(0) #remove the first object since it's full now
+        if recognizePattern(clapList):
+          print 'Would open door'
+    sampleCount = 0 #reset
+    sampleAbsSum = 0.0 #rest
+
 
 print("* done recording")
 
