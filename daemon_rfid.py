d50a0d08a3ce390962689c43081c5b5f811a5670
William Budington <bill.budington@gmail.com>
2013-04-06 20:03:05 -0700

    option to foreground rfid daemon

--- daemon_rfid.py
+++ daemon_rfid.py
@@ -6,22 +6,28 @@ import subprocess
 import time
 from os import fork, setsid, umask, dup2
 from sys import stdin, stdout, stderr
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-f","--foreground",action="store_true",dest="foreground")
+(options, args) = parser.parse_args()
 
 #GPIO.setwarnings(False)
 
 # daemonize
-if fork(): exit(0)
-umask(0)
-setsid()
-if fork(): exit(0)
-stdout.flush()
-stderr.flush()
-si = file('/dev/null', 'r')
-so = file('/dev/null', 'a+')
-se = file('/dev/null', 'a+', 0)
-dup2(si.fileno(), stdin.fileno())
-dup2(so.fileno(), stdout.fileno())
-dup2(se.fileno(), stderr.fileno())
+if not options.foreground:
+	if fork(): exit(0)
+	umask(0)
+	setsid()
+	if fork(): exit(0)
+	stdout.flush()
+	stderr.flush()
+	si = file('/dev/null', 'r')
+	so = file('/dev/null', 'a+')
+	se = file('/dev/null', 'a+', 0)
+	dup2(si.fileno(), stdin.fileno())
+	dup2(so.fileno(), stdout.fileno())
+	dup2(se.fileno(), stderr.fileno())
 
 #open codes file
 try:
@@ -33,7 +39,10 @@ except:
 	print("Could Find Code File")
 	exit() 
 
-ser = serial.Serial('/dev/ttyUSB0', 2400, timeout=1)
+try:
+	ser = serial.Serial('/dev/ttyUSB0', 2400, timeout=1)
+except Exception:
+	print "Serial device not found"
 
 lasttime = 0
 
